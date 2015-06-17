f142fbca9d9c96ba51cdb35ae34da7e6b971961b
pipo <psychedelic.nekopunch@gmail.com>
2015-06-17 11:43:36 +0900

    update array.rb

--- helloworld/array.rb
+++ helloworld/array.rb
@@ -1,9 +1,21 @@
 arr = [1, 2, 3, 4, 5]
+res = []
+
+i = 0
 
 p arr.count
+p '---------'
 
 arr.reverse!
-
 arr.each do |item|
 	p item
+	res.push(item)
+end
+
+p '---------'
+
+res.reverse!
+res.each do |item|
+	p item.to_s + '.' + i.to_s
+	i += 1
 end
